368967a5ac02da8bb01aa7ef99ef13a99859f2b5
PolKrup <117134290+PolKrup@users.noreply.github.com>
2023-02-14 01:00:30 +0700

    Add files via upload

---
+++ slideShow.js
@@ -0,0 +1,16 @@
+var stopped = true
+function slideShow(counter) {
+    if(stopped === true) {
+        return
+    }
+    if (counter < 20) {
+        setTimeout(function () {
+            plusSlides(1);
+            slideShow(counter);
+        }, 1000);
+    }
+}
+function start(currentCount) {
+    stopped = !stopped;
+    slideShow(currentCount);
+}
